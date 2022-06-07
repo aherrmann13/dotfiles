8c3ff3f56199f23e3327d791af0a3c0152e19caf
Adam Herrmann <adamherrmann13@gmail.com>
2022-06-07 05:05:02 +0000

    making installers laptop specific

--- installer.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install network-manager
-
-## e7250 broadcom card specific
-sudo apt install bcmwl-kernel-source
---
+++ laptop/e7250.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+sudo apt install bcmwl-kernel-source
